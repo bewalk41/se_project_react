1b9b888d00db71df13221d98bb6049f5cddab254
Bryan Walker <bewalk41@gmail.com>
2024-07-21 23:54:44 -0500

    ModalWithForm Added

--- src/assets/components/ItemCard/ItemCard.jsx
+++ src/assets/components/ItemCard/ItemCard.jsx
@@ -3,10 +3,10 @@ import "./ItemCard.css";
 
 function ItemCard({ item }) {
   return (
-    <div key={item.id}>
-      <h2>{item.name}</h2>
-      <img src={item.link} alt={item.name} />
-    </div>
+    <li className="card">
+      <h2 className="card__name">{item.name}</h2>
+      <img className="card__image" src={item.link} alt={item.name} />
+    </li>
   );
 }
 
